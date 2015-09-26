668c8fd97ab7d0e07ab29fdaaaa5f5d1e103c27c
sergiohdezramos <hanibalconquer@gmail.com>
2015-09-27 01:01:41 +0200

    Update LetraRandom2.php

--- LetraRandom2.php
+++ LetraRandom2.php
@@ -25,6 +25,6 @@ otra forma
 
 $nombre = "sergio";
 $longitud = strlen($name); // me da un número 
-$random = rand(0, $largo - 1);// otro número 
-$sustraer = substr($name, $random, 1);
-print $sustraer;
+$random = rand(0, $longitud - 1);// letra
+$resultado = substr($nombre, $random, 1);
+print $resultado;

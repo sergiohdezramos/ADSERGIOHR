f7fdcc8dcfc4ef031b07b774601cb9ca7ab73376
sergiohdezramos <hanibalconquer@gmail.com>
2015-09-26 23:29:19 +0200

    Create caracteraritmetico2.php

---
+++ caracteraritmetico2.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<b>
+
+	<?php
+
+$n="Retamar";
+print substr($n,rand(0,strlen($n)-1),1);
+?>
+
+	
+	
+ </b>
+	
+</body>
+</html>

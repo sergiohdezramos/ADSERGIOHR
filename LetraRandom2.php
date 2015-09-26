4b46056642d06506c2d4c54fb7db32c8f913b72e
sergiohdezramos <hanibalconquer@gmail.com>
2015-09-27 01:00:41 +0200

    Update and rename caracteraritmetico2.php to LetraRandom2.php

--- LetraRandom2.php
+++ LetraRandom2.php
@@ -20,3 +20,11 @@ print substr($n,rand(0,strlen($n)-1),1);
 	
 </body>
 </html>
+
+otra forma
+
+$nombre = "sergio";
+$longitud = strlen($name); // me da un número 
+$random = rand(0, $largo - 1);// otro número 
+$sustraer = substr($name, $random, 1);
+print $sustraer;
